5410f55fe747c8eaaed12c28c134ece25bb1e43c
Tako <tako520cn@gmail.com>
2024-07-18 10:39:33 -0400

    added

--- src/main/java/com/leetcode/IsValidBST.java
+++ src/main/java/com/leetcode/IsValidBST.java
@@ -1,28 +1,38 @@
+package com.leetcode;
+
 import java.util.Stack;
 
-import util.TreeNode;
+import com.leetcode.util.TreeNode;
 
 public class IsValidBST {
 
   public boolean isValidBST(TreeNode root) {
-    if (root == null)
+    if (root == null){
+      System.out.println();
       return true;
+    }
     Stack<TreeNode> stack = new Stack<>();
     TreeNode pre = null;
     while (root != null || !stack.isEmpty()) {
-      System.out.println("out while");
       while (root != null) {
+        System.out.printf("pushing root inner while: %d \n", root.val);
         stack.push(root);
         root = root.left;
-        System.out.println("in while");
+        System.out.printf("updated root with left node: %s \n", root != null ? root.val : "null");
       }
-      System.out.println(stack);
+      System.out.printf("current stack size: %d \n", stack.size());
       root = stack.pop();
-      if (pre != null && root.val <= pre.val)
+      System.out.printf("pre.val: %s <= root.val: %d \n", pre != null ? pre.val : "null", root.val);
+      if (pre != null && root.val <= pre.val){
+        System.out.println();
         return false;
+      }
       pre = root;
       root = root.right;
+      System.out.printf("pre.val is now: %d | updated root with right node: %s \n", pre.val, root != null ? root.val : "null");
+
     }
+    System.out.println();
     return true;
   }
 }
