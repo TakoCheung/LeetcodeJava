6bed3141169af7200001bc5cc0b2c89138bb9593
Tako <tako520cn@gmail.com>
2024-07-22 14:30:03 -0400

    added

--- src/main/java/com/leetcode/TreeUtil.java
+++ src/main/java/com/leetcode/TreeUtil.java
@@ -2,6 +2,7 @@ package com.leetcode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import com.leetcode.util.TreeNode;
 
@@ -12,11 +13,76 @@ public class TreeUtil {
     return res;
   }
 
+  public List<Integer> preorderTraversal(TreeNode root) {
+    List<Integer> res = new ArrayList<>();
+    preorderTraversalHelper(root, res);
+    return res;
+  }
+
+  public List<Integer> postorderTraversal(TreeNode root) {
+    List<Integer> res = new ArrayList<>();
+    postorderTraversalHelper(root, res);
+    return res;
+  }
+
+  public boolean isValidBST(TreeNode root) {
+    return isValidBSTHelper(root);
+  }
+
+  public TreeNode insertIntoBST(TreeNode root, int val) {
+    root = insertIntoBSTHelper(root, val);
+    return root;
+  }
+
+  // helper method
   void inorderTraversalHelper(TreeNode root, List<Integer> res) {
     if (root != null) {
       inorderTraversalHelper(root.left, res);
       res.add(root.val);
       inorderTraversalHelper(root.right, res);
     }
+  }
+
+  void preorderTraversalHelper(TreeNode root, List<Integer> res) {
+    if (root != null) {
+      res.add(root.val);
+      preorderTraversalHelper(root.left, res);
+      preorderTraversalHelper(root.right, res);
+    }
+  }
+
+  void postorderTraversalHelper(TreeNode root, List<Integer> res) {
+    if (root != null) {
+      postorderTraversalHelper(root.left, res);
+      postorderTraversalHelper(root.right, res);
+      res.add(root.val);
+    }
+  }
+
+  boolean isValidBSTHelper(TreeNode root) {
+    if (root != null) {
+      if (root.left != null && root.left.val >= root.val) {
+        return false;
+      }
+      if (root.right != null && root.right.val <= root.val) {
+        return false;
+      }
+      isValidBSTHelper(root.left);
+      isValidBSTHelper(root.right);
+    }
+    return true;
+  }
+
+  private TreeNode insertIntoBSTHelper(TreeNode temp, int val) {
+    if (temp == null) {
+      return new TreeNode(val);
+    } else {
+      if (temp.val > val)
+        temp.left = insertIntoBSTHelper(temp.left, val);
+      else
+        temp.right = insertIntoBSTHelper(temp.right, val);
+    }
+    System.gc();
+    return temp;
   }
 }
--- src/test/java/com/leetcode/TreeUtilTest.java
+++ src/test/java/com/leetcode/TreeUtilTest.java
@@ -10,9 +10,91 @@ public class TreeUtilTest {
   public void testInorderTraversal() {
     TreeUtil treeUtil = new TreeUtil();
     
+    TreeNode root = new TreeNode(2);
+    root.left = new TreeNode(1);
+    root.right = new TreeNode(3);
+    assertArrayEquals(new int []{1,2,3}, treeUtil.inorderTraversal(root).stream().mapToInt(i -> i).toArray());
+  }
+
+  @Test
+  public void testPreorderTraversal() {
+    TreeUtil treeUtil = new TreeUtil();
+    
+    TreeNode root = new TreeNode(2);
+    root.left = new TreeNode(1);
+    root.right = new TreeNode(3);
+    assertArrayEquals(new int []{2,1,3}, treeUtil.preorderTraversal(root).stream().mapToInt(i -> i).toArray());
+  }
+
+  @Test
+  public void testPostorderTraversal() {
+    TreeUtil treeUtil = new TreeUtil();
+    
+    TreeNode root = new TreeNode(2);
+    root.left = new TreeNode(1);
+    root.right = new TreeNode(3);
+    assertArrayEquals(new int []{1,3,2}, treeUtil.postorderTraversal(root).stream().mapToInt(i -> i).toArray());
+  }
+
+  @Test
+  public void testValidBST() {
+    TreeUtil treeUtil = new TreeUtil();
+    
+    // Test Case 1: Valid BST
     TreeNode root1 = new TreeNode(2);
     root1.left = new TreeNode(1);
     root1.right = new TreeNode(3);
-    assertArrayEquals(new int []{1,2,3}, treeUtil.inorderTraversal(root1).stream().mapToInt(i -> i).toArray());
+    assertTrue(treeUtil.isValidBST(root1), "Test Case 1 Failed");
+    
+    // Test Case 2: Invalid BST (left child greater than root)
+    TreeNode root2 = new TreeNode(5);
+    root2.left = new TreeNode(6);
+    root2.right = new TreeNode(7);
+    assertFalse(treeUtil.isValidBST(root2), "Test Case 2 Failed");
+    
+    // Test Case 3: Invalid BST (right child less than root)
+    TreeNode root3 = new TreeNode(10);
+    root3.left = new TreeNode(5);
+    root3.right = new TreeNode(4);
+    assertFalse(treeUtil.isValidBST(root3), "Test Case 3 Failed");
+    
+    // Test Case 4: Valid BST (single node)
+    TreeNode root4 = new TreeNode(1);
+    assertTrue(treeUtil.isValidBST(root4), "Test Case 4 Failed");
+    
+    // Test Case 5: Valid BST (larger tree)
+    TreeNode root5 = new TreeNode(8);
+    root5.left = new TreeNode(3);
+    root5.right = new TreeNode(10);
+    root5.left.left = new TreeNode(1);
+    root5.left.right = new TreeNode(6);
+    root5.left.right.left = new TreeNode(4);
+    root5.left.right.right = new TreeNode(7);
+    root5.right.right = new TreeNode(14);
+    root5.right.right.left = new TreeNode(13);
+    assertTrue(treeUtil.isValidBST(root5), "Test Case 5 Failed");
+  }
+
+  @Test
+  public void testInsertIntoBST() {
+    TreeUtil treeUtil = new TreeUtil();
+    
+    TreeNode root = new TreeNode(4);
+    root.left = new TreeNode(2);
+    root.left.left = new TreeNode(1);
+    root.left.right = new TreeNode(3);
+    root.right = new TreeNode(7);
+    treeUtil.insertIntoBST(root, 5);
+    assertArrayEquals(new int []{1,2,3,4,5,7}, treeUtil.inorderTraversal(root).stream().mapToInt(i -> i).toArray());
+
+    TreeNode root1 = new TreeNode(40);
+    root1.left = new TreeNode(20);
+    root1.left.left = new TreeNode(10);
+    root1.left.right = new TreeNode(30);
+    root1.right = new TreeNode(60);
+    root1.right.left = new TreeNode(50);
+    root1.right.right = new TreeNode(70);
+    treeUtil.insertIntoBST(root1, 25);
+    assertArrayEquals(new int []{10,20,25,30,40,50,60,70}, treeUtil.inorderTraversal(root1).stream().mapToInt(i -> i).toArray());
   }
 }
