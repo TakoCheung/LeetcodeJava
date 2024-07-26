b8cde7bf615707f20146bc7da79cb55f853bf812
Tako <tako520cn@gmail.com>
2024-07-26 13:29:00 -0400

    added

---
+++ src/test/java/com/leetcode/PrefixSumTest.java
@@ -0,0 +1,12 @@
+package com.leetcode;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class PrefixSumTest {
+  private PrefixSum testPrefixSum = new PrefixSum();
+  @Test
+  public void testFindMaxLength() {
+    assertEquals(4, testPrefixSum.findMaxLength(new int[]{0,1,1,0,1,1,1,0}));
+  }
+}
