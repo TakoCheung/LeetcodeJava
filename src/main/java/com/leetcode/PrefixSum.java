6673d3bdaeab7c721afd6059d71aa76d6026e985
Tako <tako520cn@gmail.com>
2024-07-29 14:06:04 -0400

    added

--- src/main/java/com/leetcode/PrefixSum.java
+++ src/main/java/com/leetcode/PrefixSum.java
@@ -1,7 +1,13 @@
 package com.leetcode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
 public class PrefixSum {
-  //TODO: reminder
+  // TODO: reminder
   public int findMaxLength(int[] nums) {
     int N = nums.length;
     int[] mp = new int[2 * N + 2];
@@ -17,6 +23,21 @@ public class PrefixSum {
       } else {
         result = Math.max(result, i - mp[current] + 1);
       }
+    }
+    return result;
+  }
+
+  public int subarraySum(int[] nums, int k) {
+    int sum = 0, result = 0;
+    Map<Integer, Integer> preSum = new HashMap<>();
+
+    preSum.put(0, 1);
+    for (int i = 0; i < nums.length; i++) {
+      sum += nums[i];
+      if (preSum.containsKey(sum - k)) {
+        result += preSum.get(sum - k);
+      }
+      preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
     }
     return result;
   }
--- src/test/java/com/leetcode/PrefixSumTest.java
+++ src/test/java/com/leetcode/PrefixSumTest.java
@@ -5,8 +5,14 @@ import org.junit.jupiter.api.Test;
 
 public class PrefixSumTest {
   private PrefixSum testPrefixSum = new PrefixSum();
+
   @Test
   public void testFindMaxLength() {
-    assertEquals(4, testPrefixSum.findMaxLength(new int[]{0,1,1,0,1,1,1,0}));
+    assertEquals(4, testPrefixSum.findMaxLength(new int[] { 0, 1, 1, 0, 1, 1, 1, 0 }));
+  }
+
+  @Test
+  public void testSubarraySum() {
+    assertEquals(1, testPrefixSum.subarraySum(new int[] { -1, -1, 1 }, 1));
   }
 }
