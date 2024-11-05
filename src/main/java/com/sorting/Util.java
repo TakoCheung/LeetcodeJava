17129f435241872b4b6d330b82825e0945881d34
Tako <tako520cn@gmail.com>
2024-11-05 14:46:27 -0500

    update: reorder some code

--- src/main/java/com/sorting/Util.java
+++ src/main/java/com/sorting/Util.java
@@ -9,26 +9,4 @@ public class Util {
     }
   }
 
-  public static void conquer(int[] toBeSorted, int start, int mid, int end) {
-    int[] merger = new int[end - start + 1];
-    int leftIdx = start;
-    int rightIdx = mid + 1;
-    int idx = 0;
-
-    while (leftIdx <= mid && rightIdx <= end) {
-      merger[idx++] = toBeSorted[leftIdx] <= toBeSorted[rightIdx] ? toBeSorted[leftIdx++] : toBeSorted[rightIdx++];
-    }
-    while (leftIdx <= mid) {
-      merger[idx++] = toBeSorted[leftIdx++];
-    }
-    while (rightIdx <= end) {
-      merger[idx++] = toBeSorted[rightIdx++];
-    }
-    // System.out.printf("left:%d | right:%d\n", left, right);
-    for (idx = 0; idx < merger.length; idx++) {
-      // System.out.printf("idx:%d | left:%d | merger[idx]:%d\n", idx, left,
-      // merger[idx]);
-      toBeSorted[start + idx] = merger[idx];
-    }
-  }
 }
